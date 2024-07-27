af89bb7b90c731ecc955df2bc0f5b18de250dc4c
Vipul K Mishra <Vipul.Mishra@gdit.com>
2024-07-27 12:35:31 -0400

    Adding messages

---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import { ReactiveFormsModule } from '@angular/forms';
+import { CardFormComponent } from './card-form/card-form.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CardFormComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
