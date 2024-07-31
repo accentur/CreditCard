920afa403cdc0e38f5f9ff11b1e7e0d8048ac868
Vipul K Mishra <Vipul.Mishra@gdit.com>
2024-07-31 01:45:13 -0400

    masking--self

--- src/app/card-form/card-form.component.ts
+++ src/app/card-form/card-form.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { FormGroup, FormControl } from '@angular/forms';
 
 import { Validators } from '@angular/forms';
+import { DateFormControl } from '../date-form-control';
 
 @Component({
   selector: 'app-card-form',
@@ -11,9 +12,35 @@ import { Validators } from '@angular/forms';
 export class CardFormComponent {
 
 
+onSubmit() {
+
+  
+}
+
+
   cardForm = new FormGroup({
 
-    name:new FormControl('',[Validators.required,Validators.minLength(3),Validators.maxLength(5)])
+    name: new FormControl('', [Validators.required, Validators.minLength(3), Validators.maxLength(5)]),
+    cardNumber: new FormControl('', [
+      
+      Validators.required,
+      Validators.minLength(16),
+      Validators.maxLength(16)
+    ]),
+    expiration: new DateFormControl('', [
+      
+      Validators.required,
+      Validators.pattern(/^(0[1-9]|1[0-2])\/\d{2}$/)
+
+    ]),
+    securityCode: new FormControl('', [
+      
+      Validators.required,
+      Validators.minLength(3),
+      Validators.maxLength(3)
+    ])
+    
+    
     //name:new FormControl('',[Validators.pattern(/\s/)])
   });
 
